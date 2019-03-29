eaf7b8a3071eaafe2502427e2bc3b9e7c68cc283
Santiago Lopez <santiago.lopez@i2cat.net>
2019-03-29 12:51:26 +0100

    problem with webpack exports

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
+// vendor libs
 import * as paper from 'paper'
 import { Howl } from 'howler'
 import 'normalize.css'
+
+// styles
 import '../scss/styles.scss'
+
+// modules
+import * as keyData from './keydata'
