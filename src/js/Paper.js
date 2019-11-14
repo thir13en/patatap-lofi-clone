e7cdf4c76d0914bc089b381e50fac18e29589cf1
Santiago Lopez <santiago.lopez@i2cat.net>
2019-11-14 12:37:22 +0100

    figuring out how to start paper class from js file

--- src/js/Paper.js
+++ src/js/Paper.js
@@ -3,50 +3,51 @@ import { Howl } from 'howler'
 
 export default class PaperBase {
   constructor () {
-  // this.sound = new Howl({
-  //   src: ['sounds/bubbles.mp3']
-  // });
-  //   this.circles = []
-  //   this.canvas = document.getElementById('myCanvas')
-  //   this.scope = {}
-  //   paper.install(this.scope)
-  //   paper.setup(this.canvas)
-  //   this.paper = this.scope.paper
-  //   this.addEvents()
-  // }
-  //
-  // addEvents () {
-  //   const tool = new this.paper.Tool()
-  //   tool.onKeyDown = this.onKeyDown.bind(this)
-  //   paper.view.onFrame = this.onFrame.bind(this)
-  // }
-  //
-  // onKeyDown (event) {
-  //   // this.sound.play()
-  //   const maxPoint = new paper.Point(this.paper.view.size.width, this.paper.view.size.height);
-  //   const randomPoint = new paper.Point.random()
-  //   const point = maxPoint * randomPoint
-  //
-  //   const circle = new paper.Path.Circle(point, 333)
-  //   circle.fillColor = 'red'
-  //   this.circles.push(maxPoint)
-  // }
-  //
-  // onFrame (event) {
-  //   this.circles.forEach(circle => {
-  //     circle.fillColor.hue += 1
-  //     circle.scale('.9')
-  //   })
-  // }
-  //
-  // addEvent (object, type, callback) {
-  //   if (object == null || typeof (object) === 'undefined') return
-  //   if (object.addEventListener) {
-  //     object.addEventListener(type, callback, false)
-  //   } else if (object.attachEvent) {
-  //     object.attachEvent('on' + type, callback)
-  //   } else {
-  //     object['on' + type] = callback
-  //   }
-  // }
+    this.sound = new Howl({
+     src: ['sounds/bubbles.mp3']
+    });
+    this.circles = []
+    this.canvas = document.getElementById('myCanvas')
+    this.scope = {}
+    paper.install(this.scope)
+    paper.setup(this.canvas)
+    this.paper = this.scope.paper
+    this.addEvents()
+    debugger;
+   }
+  
+   addEvents () {
+    const tool = new this.paper.Tool()
+    tool.onKeyDown = this.onKeyDown.bind(this)
+    paper.view.onFrame = this.onFrame.bind(this)
+   }
+  
+   onKeyDown (event) {
+    this.sound.play()
+    const maxPoint = new paper.Point(this.paper.view.size.width, this.paper.view.size.height);
+    const randomPoint = new paper.Point.random()
+    const point = maxPoint * randomPoint
+
+    const circle = new paper.Path.Circle(point, 333)
+    circle.fillColor = 'red'
+    this.circles.push(maxPoint)
+   }
+  
+   onFrame (event) {
+     this.circles.forEach(circle => {
+       circle.fillColor.hue += 1
+       circle.scale('.9')
+     })
+   }
+  
+   addEvent (object, type, callback) {
+     if (object == null || typeof (object) === 'undefined') return
+     if (object.addEventListener) {
+       object.addEventListener(type, callback, false)
+     } else if (object.attachEvent) {
+       object.attachEvent('on' + type, callback)
+     } else {
+       object['on' + type] = callback
+     }
+   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 // vendor libs
 import paper from 'paper'
 import * as keyBindings from './keydata'
+import PaperClass from './Paper';
 
 // styles
 import 'normalize.css'
 import '../scss/styles.scss'
+
+debugger;
+const paperClass = new PaperClass();
