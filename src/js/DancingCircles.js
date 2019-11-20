ae27e1e043dd003d1ee9699656736b0a60532536
Santiago Lopez <santiago.lopez@i2cat.net>
2019-11-20 10:15:03 +0100

    syntactic sugar

--- src/js/DancingCircles.js
+++ src/js/DancingCircles.js
@@ -1,4 +1,4 @@
-import paper from 'paper';
+import paper, { Tool, Point, Path } from 'paper';
 import keymap from './keymap';
 
 
@@ -19,17 +19,17 @@ export default class DancingCircles {
   }
 
   addEvents() {
-    const tool = new this.paper.Tool();
+    const tool = new Tool();
     tool.onKeyDown = this.onKeyDown.bind(this);
     paper.view.onFrame = this.onFrame.bind(this);
   }
 
   onKeyDown(event) {
     const sound = keymap[event.key].sound;
-    const maxPoint = new paper.Point(this.paper.view.size.width, this.paper.view.size.height);
-    const randomPoint = new paper.Point.random();
+    const maxPoint = new Point(this.paper.view.size.width, this.paper.view.size.height);
+    const randomPoint = new Point.random();
     const point = maxPoint.multiply(randomPoint);
-    const circle = new paper.Path.Circle(point, 333);
+    const circle = new Path.Circle(point, 333);
 
     circle.fillColor = keymap[event.key].color;
     sound.play(undefined, false);
@@ -40,7 +40,6 @@ export default class DancingCircles {
     this.circles.forEach(circle => {
       circle.fillColor.hue += 1;
       circle.scale('.9');
-      console.log(circle);
     });
   }
 
